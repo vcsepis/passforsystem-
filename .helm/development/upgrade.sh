fd919dec13455aebc6fa818963ec2fb0a9e60b52
Nguyen Thang <nguyenducthang.uet@gmail.com>
2022-05-08 21:13:46 +0700

    update deployment

--- .helm/development/upgrade.sh
+++ .helm/development/upgrade.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+BASE_RELEASE_NAME=paas-dev
+
 # upgrade postgresql
-helm upgrade -f postgresql.values.yaml paas-development .
+helm upgrade -f services/postgresql/values.yaml $BASE_RELEASE_NAME-postgresql services/postgresql
+helm upgrade -f services/redis/values.yaml $BASE_RELEASE_NAME-redis services/redis
