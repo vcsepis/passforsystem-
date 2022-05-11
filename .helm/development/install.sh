c5fd3d419353d3a72a74c20d05fc2d17aefd5bd3
Nguyen Thang <nguyenducthang.uet@gmail.com>
2022-05-11 22:11:43 +0700

    update helm

--- .helm/development/install.sh
+++ .helm/development/install.sh
@@ -5,3 +5,5 @@ BASE_RELEASE_NAME=paas-dev
 # install postgresql
 helm install -f services/postgresql/values.yaml $BASE_RELEASE_NAME-postgresql services/postgresql
 helm install -f services/redis/values.yaml $BASE_RELEASE_NAME-redis services/redis
+helm install -f services/porter/values.yaml $BASE_RELEASE_NAME-porter services/porter
+helm install -f services/webpack/values.yaml $BASE_RELEASE_NAME-webpack services/webpack
--- .helm/development/upgrade.sh
+++ .helm/development/upgrade.sh
@@ -3,5 +3,7 @@
 BASE_RELEASE_NAME=paas-dev
 
 # upgrade postgresql
-helm upgrade -f services/postgresql/values.yaml $BASE_RELEASE_NAME-postgresql services/postgresql
-helm upgrade -f services/redis/values.yaml $BASE_RELEASE_NAME-redis services/redis
+helm upgrade --install -f services/postgresql/values.yaml $BASE_RELEASE_NAME-postgresql services/postgresql
+helm upgrade --install -f services/redis/values.yaml $BASE_RELEASE_NAME-redis services/redis
+helm upgrade --install -f services/porter/values.yaml $BASE_RELEASE_NAME-porter services/porter
+helm upgrade --install -f services/webpack/values.yaml $BASE_RELEASE_NAME-webpack services/webpack
